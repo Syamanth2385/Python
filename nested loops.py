4dec22f2554a68979c1dccd7d6055f5c72545157
Syamanth Pasupuleti <119915931+Syamanth2385@users.noreply.github.com>
2026-02-02 11:58:15 +0530

    Created nested loops.py

---
+++ nested loops.py
@@ -0,0 +1,16 @@
+numbers = [5,2,5,2,2]
+for x_count in numbers :
+  output = ""
+  for count in range(x_count):
+    output +="x"
+  print(output)
+
+
+#this prints out Alphabet "F" in the terminal
+# now lets do a program with prints letter "L" in terminal
+numbers = [2,2,2,2,5]
+for x_count in numbers:
+  output = ""
+  for count in range(x_count):
+    output += "x"
+  print(output)
